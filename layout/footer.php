d58b37371a2322166145345eb046a38dab563af2
Michael Neth <mneth@callcap.com>
2016-09-01 10:51:20 -0500

    Add Google Analytics code, duh.

--- layout/footer.php
+++ layout/footer.php
@@ -42,6 +42,17 @@
 	</div>
 	<!-- Page End (main div) ========================================= -->
 
+	<!-- Tracking codes -->
+	<script>
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+		ga('create', 'UA-66607923-1', 'auto');
+		ga('send', 'pageview');
+	</script>
+
 	<script src="//api.callcap.com/track/webmatch.js" type="text/javascript"></script>
 	<script language="javascript">
 	var webmatch = new Webmatch({ rotate: 'Ed1-997-KTlu', instance_class: 'callcap_phone_number', phone_format: 'paren', phone_link: true, loadUtmParams: true, useUtmTermForSearch: true }).init();
