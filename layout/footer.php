98d14e9833967752732cb8ad7c8089a7d9ae31e1
Michael Neth <michael@infernocloud.com>
2016-08-31 23:58:55 -0500

    Test Mailchimp newsletter sign up

--- layout/footer.php
+++ layout/footer.php
@@ -14,9 +14,12 @@
 
 			<div class="subscribe">
 				<h1>Stay Updated</h1>
-				<form action="">
-					<input type="text" placeholder="Email">
-					<input class="btn" type="submit" value="Subscribe">
+				<form action="//lakeafton.us11.list-manage.com/subscribe/post?u=3a1cc6210ed1c2d4cd798d122&amp;id=2f0f0c5a86" name="mc-embedded-subscribe-form" target="_blank">
+					<input type="email" name="EMAIL" class="required email" id="mce-EMAIL" placeholder="Email" value="">
+					<input type="submit" class="btn" name="subscribe" value="Subscribe">
+
+					<!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+					<div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_3a1cc6210ed1c2d4cd798d122_2f0f0c5a86" tabindex="-1" value=""></div>
 				</form>
 			</div>
 
